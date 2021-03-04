31441f5f0727c1408c2109e3844fe151f8e03aff
abalsh <abdulhakim.alsharif@pipedrive.com>
2021-03-04 20:15:03 +0200

    Init & Run func

--- todo_api.go
+++ todo_api.go
@@ -1,50 +1,21 @@
-package main
+package todo_api
 
 import (
-	"context"
-	"fmt"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
+	"database/sql"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
-func prepareServer() *http.Server {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/todo-api-health", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	})
-
-	return &http.Server{
-		Addr:    ":8080",
-		Handler: mux,
-	}
+type App struct {
+	Router *mux.Router
+	DB     *sql.DB
 }
 
-func main() {
-	shutdown := make(chan os.Signal, 1)
-	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+func (a *App) Initialize(user, password, dbname string) {
 
-	httpServer := prepareServer()
-	srv := make(chan error)
-	go func() {
-		srv <- httpServer.ListenAndServe()
-	}()
+}
 
-	select {
-	case <-shutdown:
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
+func (a *App) Run(addr string) {
 
-		if err := httpServer.Shutdown(ctx); err != nil {
-			fmt.Fprintf(os.Stderr, "error on http server shutdown: %v\n", err)
-			os.Exit(1)
-		}
-
-		os.Exit(0)
-	case err := <-srv:
-		_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
-	}
 }
