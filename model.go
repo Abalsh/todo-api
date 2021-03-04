e7802f947a6c9d3c5a5a7b2c60ab882005028dee
abalsh <abdulhakim.alsharif@pipedrive.com>
2021-03-04 20:23:06 +0200

    Initial model

---
+++ model.go
@@ -0,0 +1,31 @@
+// model.go
+
+package todo_api
+
+import (
+	"database/sql"
+	"errors"
+)
+
+type goal struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+func (g *goal) getGoal(db *sql.DB) error {
+	return errors.New("NOT IMPLEMENTED")
+}
+
+func (g *goal) updateGoal(db *sql.DB) error {
+	return errors.New("NOT IMPLEMENTED")
+}
+func (g *goal) addGoal(db *sql.DB) error {
+	return errors.New("NOT IMPLEMENTED")
+}
+func (g *goal) deleteGoal(db *sql.DB) error {
+	return errors.New("NOT IMPLEMENTED")
+}
+func (g *goal) getGoals(db *sql.DB) error {
+	return errors.New("NOT IMPLEMENTED")
+}
