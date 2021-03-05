843a0a335fc87a029703ea0f7b9849d2bcb0ec1b
abalsh <abdulhakim.alsharif@pipedrive.com>
2021-03-05 16:39:26 +0200

    Write db init tests

---
+++ todo_api_test.go
@@ -0,0 +1,37 @@
+// main_test.go
+
+package todo_api
+
+import (
+	"log"
+	"os"
+	"testing"
+)
+
+var a App
+
+func TestMain(m *testing.M) {
+	a.Initialize(
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_NAME"))
+
+	ensureTableExists()
+	code := m.Run()
+	clearTable()
+	os.Exit(code)
+}
+
+func ensureTableExists() {
+	if _, err := a.DB.Exec(tableCreationQuery); err != nil {
+		log.Fatal(err)
+	}
+}
+
+const tableCreationQuery = `CREATE TABLE IF NOT EXISTS products
+(
+    id SERIAL,
+    name TEXT NOT NULL,
+    price NUMERIC(10,2) NOT NULL DEFAULT 0.00,
+    CONSTRAINT products_pkey PRIMARY KEY (id)
+)`
