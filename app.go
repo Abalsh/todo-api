bc08211198e3cd3d0029dbbc8d5b74c84be3d52b
abalsh <abdulhakim.alsharif@pipedrive.com>
2021-03-08 17:50:08 +0200

    getGoals route handler

--- app.go
+++ app.go
@@ -48,5 +48,22 @@ func (a *App) getGoal(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	respondWithJson(w, http.StatusOK, g)
+	respondWithJSON(w, http.StatusOK, g)
+}
+
+func (a *App) getGoals(w http.ResponseWriter, r *http.Request) {
+	count, _ := strconv.Atoi(r.FormValue("count"))
+	start, _ := strconv.Atoi(r.FormValue("start"))
+	if count > 10 || count < 1 {
+		count = 10
+	}
+	if start < 0 {
+		start = 0
+	}
+	goals, err := getGoals(a.DB, start, count)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusOK, goals)
 }
